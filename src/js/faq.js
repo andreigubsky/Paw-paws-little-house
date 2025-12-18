2c645bccb26b11a6fc044e41897acdea041e513a
andrei_gubsky <anrei.gubsky@gmail.com>
2025-12-18 22:59:38 +0200

    Fix bugs

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,16 @@
 import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
 
+const base = import.meta.env.BASE_URL;
 new Accordion('.accordion-container', {
     duration: 400,
     showMultiple: false,
     onOpen: (currentElement) => {
         const icon = currentElement.querySelector(".faq-icon-use");
-        icon.href.baseVal = "./img/sprite.svg#icon-close";
+        icon.href.baseVal = `${base}img/sprite.svg#icon-close`;
     },
     onClose: (currentElement) => {
         const icon = currentElement.querySelector(".faq-icon-use");
-        icon.href.baseVal = "./img/sprite.svg#icon-add";
+        icon.href.baseVal = `${base}img/sprite.svg#icon-add`;
     }
 }); 
